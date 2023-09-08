3339fe9ea468d677b3d66cb5d77cf0a612b39c3e
Ibrahim Kaiser <59397779+ikReza@users.noreply.github.com>
2023-09-08 12:53:13 +0600

    mail unsuccess-2

--- Module-14/assignment/src/app/api/login/route.js
+++ Module-14/assignment/src/app/api/login/route.js
@@ -32,7 +32,7 @@ export async function POST(req) {
     };
 
     // Send email
-    transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
 
     let cookie = await TokenCookie(email);
 
